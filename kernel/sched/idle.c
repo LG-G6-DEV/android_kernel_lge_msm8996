9a1448c2297a925e7d2dc6f9e0ab07c718d5ac5b
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2015-02-12 23:33:15 +0100

    UPSTREAM: PM / sleep: Re-implement suspend-to-idle handling
    In preparation for adding support for quiescing timers in the final
    stage of suspend-to-idle transitions, rework the freeze_enter()
    function making the system wait on a wakeup event, the freeze_wake()
    function terminating the suspend-to-idle loop and the mechanism by
    which deep idle states are entered during suspend-to-idle.
    
    First of all, introduce a simple state machine for suspend-to-idle
    and make the code in question use it.
    
    Second, prevent freeze_enter() from losing wakeup events due to race
    conditions and ensure that the number of online CPUs won't change
    while it is being executed.  In addition to that, make it force
    all of the CPUs re-enter the idle loop in case they are in idle
    states already (so they can enter deeper idle states if possible).
    
    Next, drop cpuidle_use_deepest_state() and replace use_deepest_state
    checks in cpuidle_select() and cpuidle_reflect() with a single
    suspend-to-idle state check in cpuidle_idle_call().
    
    Finally, introduce cpuidle_enter_freeze() that will simply find the
    deepest idle state available to the given CPU and enter it using
    cpuidle_enter().
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: git://git.kernel.org/pub/scm/linux/kernel/git/rafael/linux-pm
    br: linux-next
    (cherry picked from commit 3810631332465d967ba5e27ea2c7dff2c9afac6c)
    
    BUG=None
    TEST=1) Change power state of system to "freeze"
         2) Use echo freeze > /sys/power/state
         3) Check whether system wakes up on any interrupt(set rtc timer)
    
    Change-Id: I895e0649549abdff8d618fb0967f32d550fd2b39
    Signed-off-by: Jay Patel <jay.p.patel@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/282852
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Tested-by: Prathyushi Nangia <prathyushi.nangia@intel.com>

--- kernel/sched/idle.c
+++ kernel/sched/idle.c
@@ -105,6 +105,21 @@ static void cpuidle_idle_call(void)
 	rcu_idle_enter();
 
 	/*
+	 * Suspend-to-idle ("freeze") is a system state in which all user space
+	 * has been frozen, all I/O devices have been suspended and the only
+	 * activity happens here and in iterrupts (if any).  In that case bypass
+	 * the cpuidle governor and go stratight for the deepest idle state
+	 * available.  Possibly also suspend the local tick and the entire
+	 * timekeeping to prevent timer interrupts from kicking us out of idle
+	 * until a proper wakeup interrupt happens.
+	 */
+	if (idle_should_freeze()) {
+		cpuidle_enter_freeze();
+		local_irq_enable();
+		goto exit_idle;
+	}
+
+	/*
 	 * Ask the cpuidle framework to choose a convenient idle state.
 	 * Fall back to the default arch idle method on errors.
 	 */
