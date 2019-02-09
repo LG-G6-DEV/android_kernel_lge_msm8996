78dcec9cd817a230f16062958c1d9b4a2084a8a8
Alex <alexisr245@gmail.com>
2019-02-09 10:36:47 +0100

    Fix after cherry pick

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -651,6 +651,8 @@ int cpufreq_generic_init(struct cpufreq_policy *policy,
  *                         CPUFREQ STATS                             *
  *********************************************************************/
 
+#ifdef CONFIG_CPU_FREQ_STAT
+
 void acct_update_power(struct task_struct *p, cputime_t cputime);
 void cpufreq_task_stats_init(struct task_struct *p);
 void cpufreq_task_stats_alloc(struct task_struct *p);
