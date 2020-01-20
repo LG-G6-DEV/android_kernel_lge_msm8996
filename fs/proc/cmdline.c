156f7246fd341691dcc3e2390709e408fc805a6a
Sultan Alsawaf <sultan@kerneltoast.com>
2020-01-20 13:03:40 -0800

    proc: cmdline: Patch SafetyNet flags
    Userspace parses androidboot.* flags from /proc/cmdline and sets the
    ro.boot.* props accordingly, which in turn trips SafetyNet when the
    reported values are incorrect.
    
    Patch the cmdline flags checked by SafetyNet to prevent it from failing
    a device. These flags were found by extracting the latest snet.jar and
    searching for 'ro.boot.' strings.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -5,8 +5,9 @@
 #include <soc/qcom/lge/board_lge.h>
 #include <asm/setup.h>
 #include <linux/slab.h>
+#include <asm/setup.h>
 
-static char updated_command_line[COMMAND_LINE_SIZE];
+static char new_command_line[COMMAND_LINE_SIZE];
 
 static void proc_cmdline_set(char *name, char *value)
 {
@@ -15,20 +16,21 @@ static void proc_cmdline_set(char *name, char *value)
 
 	scnprintf(flag_pos_str, COMMAND_LINE_SIZE, "%s=", name);
 
-	flag_pos = strstr(updated_command_line, flag_pos_str);
+	flag_pos = strstr(new_command_line, flag_pos_str);
 	if (flag_pos) {
 		flag_after = strchr(flag_pos, ' ');
 		if (!flag_after)
 			flag_after = "";
 
-		scnprintf(updated_command_line, COMMAND_LINE_SIZE, "%.*s%s=%s%s",
-				(int)(flag_pos - updated_command_line),
-				updated_command_line, name, value, flag_after);
+		scnprintf(new_command_line, COMMAND_LINE_SIZE, "%.*s%s=%s%s",
+				(int)(flag_pos - new_command_line),
+				new_command_line, name, value, flag_after);
 	} else {
 		// flag was found, insert it
-		scnprintf(updated_command_line, COMMAND_LINE_SIZE, "%s %s=%s", updated_command_line, name, value);
+		scnprintf(new_command_line, COMMAND_LINE_SIZE, "%s %s=%s", new_command_line, name, value);
 	}
 }
+
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
@@ -37,8 +39,7 @@ static int cmdline_proc_show(struct seq_file *m, void *v)
 		proc_cmdline_set("androidboot.mode", "charger");
 	}
 #endif
-
-	seq_printf(m, "%s\n", updated_command_line);
+	seq_printf(m, "%s\n", new_command_line);
 	return 0;
 }
 
@@ -54,10 +55,38 @@ static const struct file_operations cmdline_proc_fops = {
 	.release	= single_release,
 };
 
+static void patch_flag(char *cmd, const char *flag, const char *val)
+{
+	size_t flag_len, val_len;
+	char *start, *end;
+
+	start = strstr(cmd, flag);
+	if (!start)
+		return;
+
+	flag_len = strlen(flag);
+	val_len = strlen(val);
+	end = start + flag_len + strcspn(start + flag_len, " ");
+	memmove(start + flag_len + val_len, end, strlen(end) + 1);
+	memcpy(start + flag_len, val, val_len);
+}
+
+static void patch_safetynet_flags(char *cmd)
+{
+	patch_flag(cmd, "androidboot.flash.locked=", "1");
+	patch_flag(cmd, "androidboot.verifiedbootstate=", "green");
+	patch_flag(cmd, "androidboot.veritymode=", "enforcing");
+}
+
 static int __init proc_cmdline_init(void)
 {
-	// copy it only once
-	strcpy(updated_command_line, saved_command_line);
+	strcpy(new_command_line, saved_command_line);
+
+	/*
+	 * Patch various flags from command line seen by userspace in order to
+	 * pass SafetyNet checks.
+	 */
+	patch_safetynet_flags(new_command_line);
 
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
